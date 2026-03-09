595dc0d3675b6737d3433aa6a09937aecfb780f6
ruslan <russaf05@gmail.com>
2026-03-09 13:25:36 +0500

    feat: create serviceProviderExtensions

---
+++ Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DependencyInjection.Abstraction;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DependencyInjection.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void ResolveTwiceAndCompare<T>
+            (this IServiceProvider serviceProvider, string serviceType)
+            where T : class, IHasInstanceId
+        {
+            Console.WriteLine($"\n--- {serviceType} ---");
+
+            var first = serviceProvider.GetService<T>();
+            Console.WriteLine($"First:  {first?.InstanceId}");
+
+            var second = serviceProvider.GetService<T>();
+            Console.WriteLine($"Second: {second?.InstanceId}");
+
+            Console.WriteLine(first == second ? "Same instances" : "Different instances");
+        }    
+    }
+}
